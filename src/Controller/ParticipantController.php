428c3d9d7ad05464421bce17d7e32225763308b1
Tangz <114076996+tangz10@users.noreply.github.com>
2025-10-09 11:16:15 +0200

    (*) Fix problème d'attribution du role

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -75,6 +75,13 @@ final class ParticipantController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            if ($participant->isAdministrateur() === true) {
+                $participant->setRoles(['ROLE_ADMIN']);
+            } else {
+                $participant->setRoles(['ROLE_USER']);
+            }
+
             $entityManager->flush();
 
             return $this->redirectToRoute('app_participant_show', [
