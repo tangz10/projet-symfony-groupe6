904e77136e16c41d3ddd4c6afc6642b8b4c65b44
Tangz <114076996+tangz10@users.noreply.github.com>
2025-10-09 10:00:46 +0200

    (*) Fixe Routing SortieController.php

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -15,9 +15,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/sortie')]
 class SortieController extends AbstractController
 {
-    #[Route('/', name: 'sortie_index', methods: ['GET'])]
+    #[Route(name: 'sortie_index', methods: ['GET'])]
     public function index(Request $request, SortieRepository $repo): Response
     {
         $form = $this->createForm(SortieFilterType::class);
