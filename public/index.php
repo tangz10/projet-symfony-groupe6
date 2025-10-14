9c1f2ebc511e0a02be029d81e714bae7ceb635bc
Gabriel Landry <gabriel.landry2023@campus-eni.fr>
2025-10-14 11:05:11 +0200

    archivage ok avoir heure php et symfony à la bonne heure

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+// Aligne le fuseau horaire PHP sur l'heure locale
+\date_default_timezone_set('Europe/Paris');
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
